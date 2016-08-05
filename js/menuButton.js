63d7cda267990eb49b56d41ad436a138da08d4ee
DmytroVakoliuk <divanura@gmail.com>
2016-08-05 06:21:09 +0300

    розставте марджини, щоб футер було видно

--- js/menuButton.js
+++ js/menuButton.js
@@ -1,16 +1,21 @@
 /**
  * Created by up on 30.07.16.
  */
-$(window).scroll(function() {
 
-    var vidht = window.innerWidth;
-    if (($(window).scrollTop() < 90 && vidht > 991) ||
-        $(window).scrollTop() < 300 && vidht < 992)
-    {
-        $("#min-menu").hide();
-    }
-    else{
-        $("#min-menu").show();
-    }
-
+$(document).ready(function(){
+    $(window).scroll(function() {
+        var win_widht = window.innerWidth;
+        if (($(window).scrollTop() < 90 && win_widht > 991) ||
+            $(window).scrollTop() < 300 && win_widht <= 991)
+        {
+            $(".btn-menu").hide();
+            $(".dropdown_menu_ul").hide();
+        }
+        else{
+            $(".btn-menu").show();
+        }
+    });
+    $(".btn-menu").click(function() {
+        $(".dropdown_menu_ul").toggle();
+    });
 });
